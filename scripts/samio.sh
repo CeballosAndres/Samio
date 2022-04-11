8e621b3fd2d49290136ee25b3afcc78df859a14b
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2022-04-11 15:34:33 -0500

    Add swp file exception

--- scripts/samio.sh
+++ scripts/samio.sh
@@ -27,5 +27,5 @@ run_process "$p_sam_build"
 (
 	trap 'kill 0' SIGINT
 	run_process "$p_start_api" &
-	fswatch --event Created --event Updated --event Renamed -e ".aws-sam" -e ".git" -e ".*.tmp$" -e ".*~$" -e ".*4913$" . | xargs -n 1 -I{} ~/.samio/scripts/build.sh {} "$aws_sam_path"
+	fswatch --event Created --event Updated --event Renamed -e ".aws-sam" -e ".git" -e ".*.tmp$" -e ".*~$" -e ".*4913$" -e ".*.swp$" . | xargs -n 1 -I{} ~/.samio/scripts/build.sh {} "$aws_sam_path"
 )
